ea8997e8a5e3139ac156f2c66475f0f6e7cd92da
leedongyeop <20181676@edu.hanbat.ac.kr>
2022-06-05 16:38:44 +0900

    클래스명 변경

--- src/treeTraversal/Main.java
+++ src/treeTraversal/Main.java
@@ -74,7 +74,7 @@ class Tree {
     }
 }
 
-public class Tree_Traversal {
+public class Main {
     public static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
