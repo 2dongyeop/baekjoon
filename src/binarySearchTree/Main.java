32e9c28a60f106392f6eed1bcce4942aa931164c
leedongyeop <20181676@edu.hanbat.ac.kr>
2022-06-06 22:42:05 +0900

    일부 필드 캡슐화

--- src/binarySearchTree/Main.java
+++ src/binarySearchTree/Main.java
@@ -4,15 +4,23 @@ import java.util.Scanner;
 
 class Node {
     private int data;
-    Node lNode;
-    Node rNode;
+    private Node lNode;
+    private Node rNode;
 
     public int getData() {
         return data;
     }
+    public Node getlNode() {
+        return lNode;
+    }
+    public Node getrNode() {
+        return rNode;
+    }
     public void setData(int data) {
         this.data = data;
     }
+    public void setlNode(int data) {new Node(data);}
+    public void setrNode(int data) {new Node(data);}
 
     Node(int data) { this.data = data;}
 }
@@ -22,8 +30,8 @@ class Tree {
 
     public void createNode(int data) {
         if (root == null) {
-            root.setData(data);
-            root.lNode = null; root.rNode = null;
+            root = new Node(data);
+            //root.lNode = null; root.rNode = null;
         } else {
             goToSeat(root, data);
         }
@@ -31,16 +39,16 @@ class Tree {
 
     public void goToSeat(Node node, int data) {
         if (node.getData() > data) {
-            if (node.rNode == null) {
-                node.rNode = new Node(data);
+            if (node.getrNode() == null) {
+                node.setrNode(data);
             } else {
-                goToSeat(node.rNode, data);
+                goToSeat(node.getrNode(), data);
             }
         } else {
-            if (node.lNode == null) {
-                node.lNode = new Node(data);
+            if (node.getlNode() == null) {
+                node.setlNode(data);
             } else {
-                goToSeat(node.lNode, data);
+                goToSeat(node.getlNode(), data);
             }
         }
     }
@@ -48,24 +56,28 @@ class Tree {
     public void postOrderTraversal(Node node){
         if (node != null) {
             System.out.println(node.getData());
-            if (node.lNode != null) postOrderTraversal(node.lNode);
-            if (node.rNode != null) postOrderTraversal(node.rNode);
+            if (node.getlNode() != null) postOrderTraversal(node.getlNode());
+            if (node.getrNode() != null) postOrderTraversal(node.getrNode());
         }
     }
 }
 
 public class Main {
+    final static int SIZE = 9;
     public static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         Tree tree = new Tree();
-        final int SIZE = 9;
 
         System.out.println("전위 순회 방식으로 정렬한 노드들의 데이터를 입력합니다.");
-        for (int i = 0; i < SIZE; i++) {
-            int data = scanner.nextInt();
+        try {
+            for (int i = 0; i < SIZE; i++) {
+                String data = scanner.next();
 
-            tree.createNode(data);
+                tree.createNode(Integer.parseInt(data));
+            }
+        } catch (NullPointerException e) {
+            System.err.println("NullPointerException 발생");
         }
 
         tree.postOrderTraversal(tree.root);
