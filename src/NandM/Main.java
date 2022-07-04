7c254b80b6662c8c848f4610e23a5cea89e5411d
leedongyeop <20181676@edu.hanbat.ac.kr>
2022-07-04 18:34:33 +0900

    백트래킹을 조금 더 공부한 뒤 다시 풀이할 예정입니다.

--- src/NandM/Main.java
+++ src/NandM/Main.java
@@ -8,7 +8,6 @@ public class Main {
     private static final BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
 
     public static void main(String[] args) throws IOException {
-
         //첫째 줄에 자연수 N과 M이 주어짐
         String s = bufferedReader.readLine();
         StringTokenizer st = new StringTokenizer(s);
@@ -16,20 +15,26 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        seriesNM(N, M);
+        int[] temp = new int[N + 1]; //1~N까지 쓸 예정이지만 배열은 0부터 시작이므로 N+1
+        for (int i = 0; i <= N; i++) {
+            temp[i] = i;
+        }
+
+        seriesNM(N, M, temp);
     }
 
-    private static final void seriesNM(final int N, final int M) throws IOException {
-//
-//        for (int i = 0; i < N; i++) {
-//            for (int j = 1; j <= M; j++) {
-//                bufferedWriter.write(j +" ");
-//                bufferedWriter.flush();
-//            }
-//            bufferedWriter.write("\n");
-//            bufferedWriter.flush();
-//        }
+    private static final void seriesNM(final int N, final int M, final int temp[]) throws IOException {
+        int i;
+        boolean[] visit = new boolean[N + 1];
 
-        bufferedWriter.close();
+        while (true) {
+            for (i = 1; i <= N; i++) {
+                bufferedWriter.write(i + "\n");
+
+                if (i == N) break; //for문 끝
+            }
+            bufferedWriter.flush();
+
+        }
     }
 }
