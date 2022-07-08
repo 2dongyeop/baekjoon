b4a49bd42b3a4363333d58a3a33dcbf80ec6f533
leedongyeop <20181676@edu.hanbat.ac.kr>
2022-07-08 23:12:55 +0900

    백준 - 충남대학교 프로그래밍 대회 #23055 공사장 표시판 최종 코드

--- src/constructionSiteSign/Main.java
+++ src/constructionSiteSign/Main.java
@@ -10,7 +10,7 @@ public class Main {
     private static BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) throws IOException {
-        System.out.println("표지판의 크기(N * N)에 해당하는 정수를 입력하세요");
+//        System.out.println("표지판의 크기(N * N)에 해당하는 정수를 입력하세요");
         int N = Integer.parseInt(bufferedReader.readLine());
 
         makeSiteSign(N);
@@ -19,31 +19,31 @@ public class Main {
     private static void makeSiteSign(int N) {
         if (N == 1) {
             System.out.println("*");
-        }
+        } else {
 
-        for (int i = 0; i < N; i++) {     //세로 N줄
-            System.out.print("*");  //가로 줄에서 처음 * 출력
+            for (int i = 0; i < N; i++) {     //세로 N줄
+                System.out.print("*");  //가로 줄에서 처음 * 출력
 
-            if (i == 0) {
-                for (int j = 0; j < N - 2; j++)
-                    System.out.print("*");
-            } else if (i == N - 1) {
-                for (int j = 0; j < N - 2; j++)
-                    System.out.print("*");
-            } else {
-                for (int j = 0; j < N - 2; j++) {
-                    /*
-                    * i == 1 -> j = 0, 12
-                    * i == 2 -> j = 1, 11
-                    * i == 3 -> j = 2, 10
-                    *
-                    * */
-                    if (j == i - 1 || j == N - (i + 1)) System.out.print("*");
-                    else System.out.print(" ");
+                if (i == 0) {
+                    for (int j = 0; j < N - 2; j++)
+                        System.out.print("*");
+                } else if (i == N - 1) {
+                    for (int j = 0; j < N - 2; j++)
+                        System.out.print("*");
+                } else {
+                    for (int j = 0; j < N - 2; j++) {
+                        /*
+                         * i == 1 -> j = 0, 12
+                         * i == 2 -> j = 1, 11
+                         * i == 3 -> j = 2, 10
+                         * */
+                        if (j == i - 1 || j == N - (i + 2)) System.out.print("*");
+                        else System.out.print(" ");
 
+                    }
                 }
-            }
-            System.out.println("*");  //가로 줄에서 마지막 * 출력
-        }
-    }
+                System.out.println("*");  //가로 줄에서 마지막 * 출력
+            } //end - for문
+        } //end - else문
+    } //end - makeSiteSign()
 }
