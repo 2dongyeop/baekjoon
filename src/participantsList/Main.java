0baf683f1597bb40c33cf8bddb9d2286b9d8daf7
leedongyeop <20181676@edu.hanbat.ac.kr>
2022-07-11 14:15:56 +0900

    odd, even 리스트를 만들어 학급을 따로 관리하고, Comparable 인터페이스를 이용해 정렬 시간 단축으로 인한 효율성 증가

--- src/participantsList/Main.java
+++ src/participantsList/Main.java
@@ -3,59 +3,70 @@ package participantsList;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.List;
-import java.util.PriorityQueue;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main {
-    private static PriorityQueue<Participants> list = new PriorityQueue<>();
+    private static ArrayList<Participants> odd = new ArrayList<>(); //홀수 학급
+    private static ArrayList<Participants> even = new ArrayList<>(); //짝수 학급
     private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) throws IOException {
-//        System.out.println("학급 수 N과 학급당 신청 가능한 인원수 M을 입력");
-        String str = br.readLine();
+//        System.out.println("학급 수 N과 학급당 최대 인원수 M을 입력");
+        String[] str = br.readLine().split(" ");
 
-        StringTokenizer st = new StringTokenizer(str);
-
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(str[0]);
+        int M = Integer.parseInt(str[1]);
 
         boolean isEOF = false;
 
         do {
-//        for (int i = 0; i < M; i++) {
 //            System.out.println("학급과 이름을 입력하세요");
-            String input = br.readLine();
+            String[] input = br.readLine().split(" ");
 
-            st = new StringTokenizer(input);
+            int classNumber = Integer.parseInt(input[0]);
+            String name = input[1];
 
-            String classNumber = st.nextToken();
-            String name = st.nextToken();
+            if (classNumber == 0 && name.equals("0")) isEOF = true;
 
-            if (classNumber == "0" && name == "0") isEOF = true;
-
-            int classNumberInt = Integer.parseInt(classNumber);
-
-            list.add(new Participants(classNumberInt, name));
+            if (classNumber % 2 == 0) {
+                even.add(new Participants(classNumber, name));
+            } else if (classNumber % 2 == 1) {
+                odd.add(new Participants(classNumber, name));
+            }
         } while (!isEOF);
 
-        printList(list);
+        Collections.sort(odd);
+        Collections.sort(even);
+
+        printList(odd);
+        printList(even);
     }
 
-    private static void printList(PriorityQueue<Participants> list) {
+    private static void printList(ArrayList<Participants> list) {
         for (Participants p : list) {
             System.out.println(p.classNumber + " " + p.name);
         }
     }
 }
 
-class Participants {
+class Participants implements Comparable<Participants> {
     int classNumber;
     String name;
 
     Participants(int classNumber, String name) {
         this.classNumber = classNumber;
         this.name = name;
+    }
+
+    @Override
+    public int compareTo(Participants o) {
+        if (this.classNumber == o.classNumber) {
+            if (this.name.length() == o.name.length()) {
+                return this.name.compareTo(o.name);
+            }
+            return this.name.length() - o.name.length();
+        }
+        return this.classNumber - o.classNumber;
     }
 }
