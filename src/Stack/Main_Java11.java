599145b1d1bd920df84cce920b85ddde9182679d
leedongyeop <ldy_1204@naver.com>
2022-11-11 21:46:02 +0900

    백준 : 스택 #10828번 Java11, Java17 정답 코드

---
+++ src/Stack/Main_Java11.java
@@ -0,0 +1,72 @@
+package Stack;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main_Java11 {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static Stack<Integer> stack = new Stack<>();
+    private static StringTokenizer stringTokenizer;
+
+    public static void main(String[] args) throws IOException {
+
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            String input = br.readLine();
+            classifyCommands(input);
+        }
+    }
+
+    private static void classifyCommands(String input) {
+
+        stringTokenizer = new StringTokenizer(input);
+
+        if (stringTokenizer.countTokens() == 2) {
+            stringTokenizer.nextToken();
+            stack.push(Integer.parseInt(stringTokenizer.nextToken()));
+        } else {
+            classifyElse(input);
+        }
+    }
+
+    private static void classifyElse(String input) {
+
+        switch (input) {
+            case "pop" : {
+                if (stack.empty()) System.out.println("-1");
+                else
+                    System.out.println(stack.pop());
+
+                break;
+            }
+            case "size" : {
+                System.out.println(stack.size());
+                break;
+            }
+            case "top" : {
+                if (stack.empty()) System.out.println("-1");
+                else
+                    System.out.println(stack.peek());
+
+                break;
+            }
+            case "empty" : {
+                System.out.println(validateEmpty(stack.empty()));
+                break;
+            }
+        }
+    }
+
+    private static int validateEmpty(boolean b) {
+
+        if (b) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
