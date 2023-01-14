d5974533c17912c2fef6ad0334147213b219316e
leedongyeop <ldy_1204@naver.com>
2023-01-14 12:01:22 +0900

    백준 : 약수의 합2 (원티드 주관 코딩테스트 시험 환경 점검 문제)

---
+++ src/sumofdivisor2/Main.java
@@ -0,0 +1,20 @@
+package sumofdivisor2;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        long answer = 0;
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= N; i++) {
+            answer += i * (N / i);
+        }
+
+        System.out.println(answer);
+    }
+}
