b48f2b6c7e8876a412887b00eca5c0b2da1ddff6
leedongyeop <ldy_1204@naver.com>
2023-01-30 10:37:15 +0900

    feat: 고양이 #10171

---
+++ src/cat/Main.java
@@ -0,0 +1,16 @@
+package cat;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("\\    /");
+        System.out.println("\\");
+
+        System.out.print(" )");
+        System.out.println("  ( ')");
+
+        System.out.println("(  /  )");
+
+        System.out.print(" \\");
+        System.out.println("(__)|");
+    }
+}
