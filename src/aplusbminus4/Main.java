a39151800fcad3798a383a6e4bcf81b340100417
leedongyeop <ldy_1204@naver.com>
2023-02-03 20:23:55 +0900

    feat: A + B - 4 #10951, 메모리: 14324KB, 시간: 136ms

---
+++ src/aplusbminus4/Main.java
@@ -0,0 +1,20 @@
+package aplusbminus4;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(final String... args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        String input = "";
+
+        while((input = br.readLine()) != null) {
+            st = new StringTokenizer(input);
+
+            System.out.println(Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken()));
+        }
+    }
+}
