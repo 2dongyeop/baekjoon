3149a8bdef5325ac6d638451aad5f0131a800429
leedongyeop <ldy_1204@naver.com>
2023-02-06 13:33:18 +0900

    feat: 숫자의 합 #11720, 메모리: 14236KB, 시간: 132ms

---
+++ src/boj_11720/Main.java
@@ -0,0 +1,21 @@
+package boj_11720;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        String input = br.readLine();
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum += Integer.parseInt(String.valueOf(input.charAt(i)));
+        }
+
+        System.out.println(sum);
+    }
+}
