dd00adb3aa8da3a26cbba1f48beed618304f99fa
leedongyeop <ldy_1204@naver.com>
2023-02-22 22:31:09 +0900

    feat: 좌표 정렬하기2 #11651, 메모리: 57676KB, 시간: 1444ms

---
+++ src/boj_11651/Main.java
@@ -0,0 +1,48 @@
+package boj_11651;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+
+        Node[] nodes = new Node[n];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            nodes[i] = new Node(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+        }
+
+        Arrays.sort(nodes, new Comparator<Node>() {
+            @Override
+            public int compare(Node o1, Node o2) {
+                if (o1.y != o2.y) {
+                    return o1.y - o2.y;
+                } else {
+                    return o1.x - o2.x;
+                }
+            }
+        });
+
+        for (Node node : nodes) {
+            System.out.println(node.x + " " + node.y);
+        }
+    }
+
+    static class Node {
+        final int x;
+        final int y;
+
+        public Node(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+}
