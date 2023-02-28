627cb4fd72ca8cb35a83fafb90feb059e6acd1fb
leedongyeop <ldy_1204@naver.com>
2023-02-28 20:03:32 +0900

    feat: 피보나치 수5 #10870, 메모리: 14216KB, 시간: 132ms

---
+++ src/boj_10870/Main.java
@@ -0,0 +1,34 @@
+package boj_10870;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        if (n == 0) {
+            System.out.println(0);
+            return;
+        } else if (n == 1) {
+            System.out.println(1);
+            return;
+        } else if (n == 2) {
+            System.out.println(1);
+            return;
+        }
+
+        int[] array = new int[n + 1];
+        array[0] = 0;
+        array[1] = 1;
+
+        for (int i = 2; i <= n; i++) {
+            array[i] = array[i - 1] + array[i - 2];
+        }
+
+        System.out.println(array[n]);
+    }
+}
